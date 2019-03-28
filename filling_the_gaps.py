f50b748626fed59d1f5ddea6aa2d60346affda80
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-03-28 18:45:50 +0100

    first commit

---
+++ filling_the_gaps.py
@@ -0,0 +1,15 @@
+#! python3
+
+'''filling_the_gaps.py - A program that finds all files with a given prefix, such as spam001.txt, spam002.txt, and so on,
+in a single folder and locates any gaps in the numbering (such as if there is a spam001.txt and spam003.txt but no spam002.txt).
+Program rename all the later files to close this gap.
+'''
+
+# TODO:
+# TODO:
+# TODO: 
+# TODO:
+# TODO:
+# TODO:
+# TODO:
+# TODO:
