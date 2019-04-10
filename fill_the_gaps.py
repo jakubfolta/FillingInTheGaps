49bdd758c5a41cbe3948e3efe654b8c380397bde
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-10 08:33:18 +0200

    solidifying knowledge

--- fill_the_gaps.py
+++ fill_the_gaps.py
@@ -1,3 +1,15 @@
 #!python3
 
-''''''
+'''fill_the_gaps.py - A program that finds all files with a given prefix, such as spam001.txt, spam002.txt, and so on,
+in a single folder and locates any gaps in the numbering (such as if there is a spam001.txt and spam003.txt but no spam002.txt).
+Program rename all the later files to close this gap.'''
+
+# TODO: Import essential modules.
+# TODO: Set directory to check.
+# TODO: Create regex.
+# TODO: Use for loop and os.listdir() to check files.
+# TODO: Create new filename with while loop.
+# TODO:
+# TODO:
+# TODO:
+# TODO:
