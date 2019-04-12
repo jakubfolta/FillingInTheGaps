7610f7757d850219caa18fa07ba5d4c8ebe23e24
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-12 13:27:59 +0200

    refactoring-solidifying knowledge

--- fill_the_gaps.py
+++ fill_the_gaps.py
@@ -57,7 +57,7 @@ for file in os.listdir(dir_to_check):
         num += 1
 
 # Check files.
-    if match.group(2) == str(int(match.group(2))) and int(match.group(2)) in range_for_filenumbers:
+    if number == str(int(number)) and int(number) in range_for_filenumbers:
         print('File in range for filenumbers.')
         continue
     else:
